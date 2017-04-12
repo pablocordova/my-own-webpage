c48ffe297a7c71063e4382c85997174ab3632e95
Pablo Cordova Morales <pccm_77@hotmail.com>
2017-04-12 13:40:34 -0500

    Slow scroll when click in some section in navbar

--- public/js/myJs.js
+++ public/js/myJs.js
@@ -2,3 +2,20 @@ var video = $('#video-welcome')[0];
 video.loop = true;
 video.load();
 
+/**
+ * To animate the scroll when click in one secction of navbar
+ */
+$(document).ready(function() {
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            // It seems like 800 ms is optimum to the movement
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top    
+            }, 800, function() {
+                window.location.hash = hash;
+            });
+        }
+    });
+});
